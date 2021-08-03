7d5bc18269d79bafa9c9cbbcd4e15cce6bd8b1f0
Beckyace <85867708+Beckyace@users.noreply.github.com>
2021-08-03 11:49:14 +0300

    Add files via upload

---
+++ MorningPhpOne/file_one.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hello world";
+echo "<br>";
+echo "Good morning team";
+echo "<br>";
+echo 10;
+echo "<br>";
+echo 25+58;
+echo "<br>";
+echo "25+58=".(25+58);
+echo "<br>";
+echo "25+58=". 58;
+echo "<br>";
+echo "Good morning class your mean grade is ". (10+10.5);
+echo "<br>";
+//hello there (12.5+10) is equivalent to (22.5-10.0)
+echo "Hello there,".(12.5+10)." is equivalent to ". (32.5-10.0);
+echo "<br>";
+//(90-5) is the mean for our class and (56+3) is for your class
+echo (90-5)." is the mean for our class and ".(56+3)." is for your class";
---
+++ MorningPhpOne/file_two.php
@@ -0,0 +1,23 @@
+<?php
+//variable
+
+$x=20;
+$y=40;
+$z=$x + $y;
+echo $z;
+echo "<br>";
+
+//Calculate the area of a circle
+$radius = 2400;
+$area = 3.142 * $radius * $radius;
+echo "Hey, your are is ".$area;
+
+//Calculate the simple interest of a loan borrowed
+echo "<br>";
+
+$p=10000;
+$r=0.045;
+$t=3;
+
+$sp=$p*$r*$t;
+echo "Hey, your simple interest is " .$sp;
